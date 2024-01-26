f3bf62c6c055a0b9abe9462384f8b01f2dc37eaf
Hristina Glavcheva <glavchevahristina@gmail.com>
2024-01-26 21:57:09 +0200

    Create ChatController

---
+++ ChatApp/ChatApp/Controllers/ChatController.cs
@@ -0,0 +1,45 @@
+﻿using ChatApp.Models.Chat;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ChatApp.Controllers
+{
+    public class ChatController : Controller
+    {
+        private static IList<KeyValuePair<string, string>> messages =
+            new List<KeyValuePair<string, string>>();
+
+        public IActionResult Show()
+        {
+            if (messages.Count < 1)
+            {
+                return View(new ChatViewModel());
+            }
+
+            var chatViewModel = new ChatViewModel()
+            {
+                Messages = messages.Select(m => new MessageViewModel
+                {
+                    Sender = m.Key,
+                    MessageText = m.Value
+                }).ToList()
+            };
+
+            return View(chatViewModel);
+        }
+
+        [HttpPost]
+        public IActionResult Send(ChatViewModel chat)
+        {
+            if(!ModelState.IsValid)
+            {
+                return RedirectToAction(nameof(Show));
+            }
+
+            var newMessage = chat.CurrentMessage;
+
+            messages.Add(new KeyValuePair<string, string>(newMessage.Sender, newMessage.MessageText));
+            
+            return RedirectToAction(nameof(Show));
+        }
+    }
+}
