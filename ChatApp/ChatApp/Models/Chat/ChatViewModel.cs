d796697dada1be695df66cfe0c5d4ca6c161549e
Hristina Glavcheva <glavchevahristina@gmail.com>
2024-01-26 22:47:21 +0200

    Add view

--- ChatApp/ChatApp/Models/Chat/ChatViewModel.cs
+++ ChatApp/ChatApp/Models/Chat/ChatViewModel.cs
@@ -2,8 +2,13 @@
 {
     public class ChatViewModel
     {
+        public ChatViewModel()
+        {
+            Messages = new HashSet<MessageViewModel>();
+        }
+
         public MessageViewModel CurrentMessage { get; set; } = null!;
 
-        public ICollection<MessageViewModel> Messages { get; set; } = null!;    
+        public ICollection<MessageViewModel> Messages { get; set; }   
     }
 }
