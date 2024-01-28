875c5ec8e16c32cce79a2c0be7f64c24ad521f5a
Hristina Glavcheva <glavchevahristina@gmail.com>
2024-01-28 15:02:24 +0200

    Create simple TextSplitterApp

---
+++ TextSplitter/TextSplitter/Controllers/HomeController.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using TextSplitter.Models;
+
+namespace TextSplitter.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index(TextViewModel model)
+        {
+            return View(model);
+        }
+
+        [HttpPost]
+        public IActionResult Split(TextViewModel model)
+        {
+            if(String.IsNullOrWhiteSpace(model.Text))
+            {
+                return View(nameof(Index), new TextViewModel());
+            }
+
+            var splittedText = model.Text
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                .ToArray();
+
+            model.SplitText = String.Join(Environment.NewLine, splittedText);
+            
+            return RedirectToAction(nameof(Index), model);
+        }
+    }
+}
+
---
+++ TextSplitter/TextSplitter/Models/TextViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TextSplitter.Models
+{
+    public class TextViewModel
+    {
+        [Required(ErrorMessage ="Here you may enter your text to be splitted.")]
+        [StringLength(30, MinimumLength = 2, ErrorMessage = "Your text should be between 2 and 30 characters long.")]
+        public string Text { get; set; } = string.Empty;
+
+        public string SplitText { get; set; } = string.Empty;
+    }
+}
