4f150341391308308248109299d1a8b3d113e85a
Hristina Glavcheva <glavchevahristina@gmail.com>
2024-02-05 14:31:45 +0200

    Create ShoppingListApp

---
+++ ShoppingListApp/ShoppingListApp/Data/Models/Product.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ShoppingListApp.Data.Models
+{
+    public class Product
+    {
+        public Product()
+        {
+            ProductNote = new List<ProductNote>();
+            Name = string.Empty;
+        }
+        
+        public int Id { get; set; }
+
+        [Required]
+        [MaxLength(100)]
+        public string Name { get; set; }
+
+        public IList<ProductNote> ProductNote { get; set; }
+    }
+}
---
+++ ShoppingListApp/ShoppingListApp/Data/Models/ProductNote.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ShoppingListApp.Data.Models
+{
+    public class ProductNote
+    {
+        public int Id { get; set; }
+
+        [Required]
+        [MaxLength(150)]
+        public string Content { get; set; } = String.Empty;
+
+        public int ProductId { get; set; }
+
+        [ForeignKey(nameof(ProductId))]
+        public Product Product { get; set; } = null!;
+    }
+}
