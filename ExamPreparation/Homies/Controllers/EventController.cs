092adaf6488f73da7f39edb0eb2629752067caae
Hristina Glavcheva <glavchevahristina@gmail.com>
2024-02-13 16:30:40 +0200

    Create Action All

---
+++ ExamPreparation/Homies/Controllers/EventController.cs
@@ -0,0 +1,33 @@
+﻿using Homies.Data;
+using Homies.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace Homies.Controllers
+{
+    public class EventController : Controller
+    {
+        private readonly HomiesDbContext dbContext;
+
+        public EventController(HomiesDbContext _dbContext)
+        {
+            dbContext = _dbContext;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> All()
+        {
+            var model = await dbContext.Events
+                .Select(e => new EventsAllViewModel()
+                {
+                    Id = e.Id,
+                    Name = e.Name,
+                    Type = e.Type.Name,
+                    Start = e.Start.ToString(),
+                    Organiser = e.Organiser.UserName
+                }).ToListAsync();   
+            
+            return View(model);
+        }
+    }
+}
---
+++ ExamPreparation/Homies/ViewModels/EventsAllViewModel.cs
@@ -0,0 +1,36 @@
+﻿using static Homies.Data.DataConstants;
+
+namespace Homies.ViewModels
+{
+    public class EventsAllViewModel
+    {
+        public EventsAllViewModel()
+        {
+            
+        }
+
+        public EventsAllViewModel(
+            int id,
+            string name,
+            DateTime start,
+            string type,
+            string organiser)
+        {
+            Id = id;
+            Name = name;
+            Start = start.ToString(DateTimeFormat);
+            Type = type;
+            Organiser = organiser;
+        }
+
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string Start { get; set; }
+
+        public string Type { get; set; }
+
+        public string Organiser { get; set; }
+    }
+}
